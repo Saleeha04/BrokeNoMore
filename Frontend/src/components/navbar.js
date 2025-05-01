40f5818a1e6023db1131c3de77f6f85335c93ac3
Saleeha <saleeha.naveed04@gmail.com>
2025-05-01 20:33:05 +0500

    Project Structure Created
    Zoha's files have been added as well.

---
+++ Frontend/src/components/navbar.js
@@ -0,0 +1,4 @@
+//components like navbar/side menu/ or anything which is re-used within all the
+//pages should come here.
+//it's ok if this remains empty, but it'll be good if you guys separate things into files
+//for better readability ~
