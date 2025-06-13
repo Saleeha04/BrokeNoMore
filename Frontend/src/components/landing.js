05a76e2bb8d2ea6c9587c9e370a6c18ff8780175
manal-zahra <manalz1619@gmail.com>
2025-06-13 08:38:53 -0700

    landing page

--- Frontend/src/components/landing.js
+++ Frontend/src/components/landing.js
@@ -54,3 +54,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+    // const reveals = document.querySelectorAll('.reveal');
+
+    // const revealOnScroll = () => {
+    //   const windowHeight = window.innerHeight;
+    //   const revealPoint = 100;
+
+    //   reveals.forEach((el) => {
+    //     const revealTop = el.getBoundingClientRect().top;
+    //     if (revealTop < windowHeight - revealPoint) {
+    //       el.classList.add('active');
+    //     } else {
+    //       el.classList.remove('active');
+    //     }
+    //   });
+    // };
+
+    // window.addEventListener('scroll', revealOnScroll);
+    // window.addEventListener('load', revealOnScroll);
