f0997ff1fb8e9ac8f33b2bf5321e4877071b1783
Ma-seel <179339723+Ma-seel@users.noreply.github.com>
2025-06-17 09:30:45 +0500

    Login connectivity with backend

--- Frontend/src/components/login.js
+++ Frontend/src/components/login.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.signup-form');
+  const form = document.querySelector('.login-form');
+
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     try {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('http://localhost:5000/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
